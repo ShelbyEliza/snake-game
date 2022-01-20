cc77952ac04a00326f91dd3125d1d601d4f3a796
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-01-20 15:57:49 -0600

    GameBoard now renders

---
+++ my-app/src/BoardSelection.js
@@ -0,0 +1,27 @@
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { GameBoard } from "./GameBoard"
+
+// const BoardSelection = () => {
+
+//   useEffect((e) => {
+//     switch (e.target.value) {
+//       case 6:
+//         gameBoard.width("300px");
+//         gameBoard.height("300px");
+//         break;
+//       case 7:
+//         gameBoard.width("350px");
+//         gameBoard.height("350px");
+//         break;
+//       case 8:
+//         gameBoard.width("400px");
+//         gameBoard.height("400px");
+//         break;
+//       default:
+//         gameBoard.width("300px");
+//         gameBoard.height("300px");
+//     }
+//   });
+
+// }
--- my-app/src/GameBoard.js
+++ my-app/src/GameBoard.js
@@ -1,47 +1,105 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import ReactDOM from "react-dom";
 
 const GameBoard = () => {
-  const [size, setSize] = useState(6);
-  const gameBoard = document.getElementsByClassName("gameBoard");
+  const [rows, setRow] = useState(6);
+  const [cols, setCol] = useState(6);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  var counterRow = -1;
+  var spliceValue = 0;
 
-    console.log(gameBoard);
+  const gameBoard = new Array(rows);
 
-    // let boardSize = { size } * { size };
+  for (var i = 0; i < gameBoard.length; i++) {
+    gameBoard[i] = new Array(cols);
+  }
+  console.log(gameBoard);
 
-    // gameBoard.style.setProperty("--grid-rows", { size });
-    // gameBoard.style.setProperty("--grid-cols", { size });
+  for (let row = 0; row < rows; row++) {
+    counterRow++;
+    for (let col = 0; col < cols; col++) {
+      if (counterRow === row) {
+        if (spliceValue === rows) {
+          spliceValue = 0;
+        }
+        // console.log(`spliceValue: ${spliceValue}, counterRow: ${counterRow} || row: ${row}, col: ${col}`);
+        console.log(
+          `spliceValue: ${spliceValue}, counterRow: ${counterRow} || row: ${row}, col: ${col}`
+        );
+        gameBoard[row].splice(
+          spliceValue,
+          1,
+          <div className="cell" key={row.toString() + "-" + col.toString()}>
+            {row + 1}, {col + 1}
+          </div>
+        );
 
-    // for (let i = 0; i < boardSize; i++) {
-    //   // let cell = <div className="grid-cell">{i + 1}</div>;
-    //   let cell = document.createElement("div");
-    //   cell.innerText = i + 1;
-    //   gameBoard.appendChild(cell);
-    // }
-  };
+        spliceValue++;
+      }
+    }
+  }
+
+  // useEffect((e) => {
+  //   switch (e.target.value) {
+  //     case 6:
+  //       gameBoard.width("300px");
+  //       gameBoard.height("300px");
+  //       break;
+  //     case 7:
+  //       gameBoard.width("350px");
+  //       gameBoard.height("350px");
+  //       break;
+  //     case 8:
+  //       gameBoard.width("400px");
+  //       gameBoard.height("400px");
+  //       break;
+  //     default:
+  //       gameBoard.width("300px");
+  //       gameBoard.height("300px");
+  //   }
+  // });
+
+  console.log(gameBoard);
 
   return (
     <div className="container">
-      <form onSubmit={handleSubmit}>
+      <form onChange={useEffect}>
         <label>Pick Your GameBoard Size:</label>
         <select
-          className="size"
-          value={size}
-          onChange={(e) => setSize(e.target.value)}
+          className="row-col-selection"
+          value={rows}
+          // onChange={handleChange}
+          onChange={(e) => {
+            setRow(e.target.value);
+            setCol(e.target.value);
+          }}
         >
           <option value={6}>6 x 6</option>
           <option value={7}>7 x 7</option>
           <option value={8}>8 x 8</option>
         </select>
-        <button>Enter</button>
       </form>
-      <div className="gameBoard">GameBoard Goes Here</div>
+      <div className="gameBoard-title">GameBoard Goes Below</div>
+      <div className="gameBoard-container">
+        <div className="gameBoard">{gameBoard}</div>
+      </div>
     </div>
   );
 };
 
+// gameBoard width = number of rows/cols * width of cells;
 export default GameBoard;
+
+// if (e.target.value === 6) {
+//   gameBoard.width("300px")
+//   gameBoard.height("300px")
+// }
+// if (e.target.value === 7) {
+//   gameBoard.width("350px")
+//   gameBoard.height("350px")
+// }
+// if (e.target.value === 8) {
+//   gameBoard.width("300px")
+//   gameBoard.height("300px")
+// }
