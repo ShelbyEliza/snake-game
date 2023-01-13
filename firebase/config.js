62bfa02306a1525d20454ac0af51e47b4bf4cc8e
Shelby Shipley <shelbyelizashipley@gmail.com>
2023-01-13 09:35:04 -0800

    add hosting and GitHub actions

---
+++ firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnOb7k3Bt7IDX6ap2UViwE0llVN99-IyI",
+  authDomain: "snake-game-27ff9.firebaseapp.com",
+  projectId: "snake-game-27ff9",
+  storageBucket: "snake-game-27ff9.appspot.com",
+  messagingSenderId: "800532962062",
+  appId: "1:800532962062:web:520b31a88babd283dad55f",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
